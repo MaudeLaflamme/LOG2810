9107b0aaabf3549563c5a8e9754ca3a53901c181
Maude Laflamme <maudelaflamme13@gmail.com>
2018-10-28 14:18:20 -0400

    ajout de fichiers java

---
+++ graphe.java
@@ -0,0 +1,23 @@
+// classe graphe 
+public class Graphe 
+{
+    /* Attributs */
+    Clinique[] carte;
+    
+
+
+    /* Constructeur*/
+    Graphe()
+    {
+
+    }
+    public Graphe creerGraphe(string nomFichier)
+    {
+        //Écrire méthode creerGraphe
+    }
+    public void lireGraphe()
+    {
+        //Écrire méthode lireGraphe()
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,14 @@
+
+public class Main 
+{
+	/**
+	 * Fonction principale
+	 * @param args (non utilise)
+	 */
+	public static void main(String[] args) 
+	{
+
+        
+    }
+
+}
