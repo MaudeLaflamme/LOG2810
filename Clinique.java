577b0bf851dd2d80ddf8a233904b386b64bc1cdb
Maude Laflamme <maudelaflamme13@gmail.com>
2018-11-01 16:43:12 -0400

    Création classe voisin

---
+++ Clinique.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+//classe Clinique
+public class Clinique
+{
+    
+    /* Attributs */
+    boolean recharge;
+    ArrayList<Voisin> voisins = new ArrayList<Voisin>();
+    
+    /* Constructeur par param�tre*/
+    Clinique(int recharge)
+    {
+    	if(recharge == 1)
+    		this.recharge = true;
+    	else
+    		this.recharge = false;
+    	voisins = new ArrayList<Voisin>();
+    }
+    
+    void addVoisin(Voisin voisin) {
+    	
+    	this.voisins.add(voisin);
+    }
+    
+    ArrayList<Voisin> getVoisins() {
+    	return this.voisins;
+    }
+
+	
+
+
+}
---
+++ Voisin.java
@@ -0,0 +1,11 @@
+
+public class Voisin {
+	        int numIdentification;
+	        int distance; 
+	    
+	        /* Constructeur par param�tre de voisins */
+	        Voisin(int numIdentification, int dist){
+	       	this.numIdentification = numIdentification;
+	       	this.distance = dist;
+	        }
+	    }
