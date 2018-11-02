42fe8cfda75f685816b458443888dc91530f4428
William Balea <william.balea@polymtl.ca>
2018-11-01 20:25:33 -0400

    ajout de section b du main

---
+++ Dijkstra.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Dijkstra {
+	
+	Dijkstra() {
+	}
+	
+	void plusCourtChemin(int reponse[][], ArrayList<Clinique> carte) {
+		
+		int index = 0;
+		int min = Integer.MAX_VALUE;
+		
+		// 1 - examine shortest unvisited neighbors (bool)
+		for (int i = 0 ; i < carte.size(); i++)
+			if (reponse[i][0] < min && carte.get(i).isVisited() == false) {
+				index = i;
+				min = reponse[i][0];
+			}
+		
+		// 2 - calculate distance between point and neighbors
+		// 3 - if shortest distance less than current -> update
+		for (int i = 0; i < carte.get(index).getVoisins().size(); i++) {
+			int newDistance = reponse[index][0] + carte.get(index).getVoisins().get(i).temps;
+			int voisinNameInNum = carte.get(index).getVoisins().get(i).numIdentification - 1;
+			if (newDistance < reponse[voisinNameInNum][0]) {
+				reponse[voisinNameInNum][0] = newDistance;
+				reponse[voisinNameInNum][1] = index;
+			}
+		}
+		
+		// 4 - le 0 (1) est visité (bool = true)
+		carte.get(index).setVisited(true);
+	}
+	
+	String ordre(int tableau[][], int depart, int arrive) {
+		if (arrive == depart)
+			return Integer.toString(depart + 1);
+		else
+			return ordre(tableau, depart, tableau[arrive][1]) + " -> " + Integer.toString(arrive + 1);
+	}
+}
