d6a5c51d69fc65fd7c713fc563e8a38e374ad841
William Balea <williambalea27@gmail.com>
2018-12-03 17:07:57 -0500

    ajout de descriptions, ui not resizable

--- TP2/Main.java
+++ TP2/Main.java
@@ -3,12 +3,10 @@ import java.io.IOException;
 public class Main{
 	
 	public static void main(String[] args) throws IOException{
-		
 			WordUI w = new WordUI();
 			Lexique lexique = new Lexique(w);
-			String nomFichier = "./lexique 2.txt";
+			String nomFichier = "./src/lexique6.txt";
 			lexique.openFiles(nomFichier);
 			lexique.readFiles();
-			//lexique.closeFiles();
 		}
 	}
--- TP2/WordUI.java
+++ TP2/WordUI.java
@@ -1,32 +1,50 @@
+/**
+ *  WordUI.java
+ *  Implementation of the Class WordUI
+ *  Original author: Balea, Laflamme and Pak
+ *  Last modified : 3 dec 2018
+ **/
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import javax.swing.*;
 
+@SuppressWarnings("serial")
 public class WordUI extends JFrame implements ActionListener {
 	JPanel p = new JPanel();
-	JButton b = new JButton("Rechercher");
-	JTextField t = new JTextField("", 20);
+	
 	JLabel l = new JLabel("Quel mot du lexique voulez-vous chercher?");
 	
+	JTextField t = new JTextField("", 20);
+	JButton b = new JButton("Rechercher");
+	
+	JLabel d = new JLabel("5 derniers mots recherches : ");
+	
+	// a represente les mots trouvés du lexique, relie au list qui est dans l'interface
 	DefaultListModel<String> dlm = new DefaultListModel<String>();
-	JList list = new JList(dlm);
+	JList<String> list = new JList<String>(dlm);
 	JScrollPane s = new JScrollPane(list);
 	ArrayList<Mots> a = new ArrayList<Mots>();
 	
+	// motsPlusUtilises relie au listRecent qui est affiche dans l'interface
 	DefaultListModel<String> dlmRecent = new DefaultListModel<String>();
-	JList listRecent = new JList(dlmRecent);
+	JList<String> listRecent = new JList<String>(dlmRecent);
 	JScrollPane sRecent = new JScrollPane(listRecent);
 	ArrayList<Mots> motsPlusUtilises = new ArrayList<Mots>();
 	
+	// utile pour avoir acces a Mots, memoire == a
 	ArrayList<Mots> memoire = new ArrayList<Mots>();
 	
 	
 
-		
+	/**
+     * Actions a faire a l'appui du bonton :
+     *  - incrementer nbUtilisation
+     *  - afficher les labels dans un popup
+     *  - ajouter mot recherche dans motsPlusUtilises
+     * @param event est le clic du bouton
+     */
 	public void actionPerformed(ActionEvent event) {
 		memoire.get(0).setNbUtilisations();
 		
@@ -41,20 +59,23 @@ public class WordUI extends JFrame implements ActionListener {
 	}
 			
 
-	
+	/**
+     * Constructeur. Cree l'interface et ajoute les elements du ui.
+     */
 	public WordUI() {
 			super("Word UI ver.2");
 			
 			setSize(400,400);
-			setResizable(true);
+			setResizable(false);
 			
 		
 			b.addActionListener(this);
 			
-			p.add(l); // entrée utilisateur
-			p.add(t); // boite de mots
-			p.add(b); // boutons
-			p.add(s); //
+			p.add(l);
+			p.add(t); 
+			p.add(b);
+			p.add(s);
+			p.add(d);
 			p.add(sRecent);
 			add(p);
 			
@@ -64,6 +85,11 @@ public class WordUI extends JFrame implements ActionListener {
 			
 		}
 	
+	/**
+     * Met a jour le tableau a dans l'interface. On clear a a chaque fois pour
+     * ne pas afficher les elements en double. memoire permet de se rappeler 
+     * du tableau a pour l'appel dans actionPerformed
+     */
 	public void update() {
 		this.dlm.removeAllElements();
 		for (Mots element : a) {
@@ -76,6 +102,9 @@ public class WordUI extends JFrame implements ActionListener {
 		this.a.clear();
 	}
 	
+	/***
+     * Met a jour le tableau motsPlusUtilises
+     */
 	public void updateWithoutClear() {
 		this.dlmRecent.removeAllElements();
 		for (Mots element : this.motsPlusUtilises) {
